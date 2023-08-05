f06943e2baf2535786db952a47b9eebfef824361
sayantan <sayantan.roy123789@gmail.com>
2023-08-05 23:32:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { SiAirbnb } from 'react-icons/si';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='main'>
+        <h1 className='contain'>The Evolution of <br/> Airbnb`s Frontend</h1>
+        <h2>@spikebrehm</h2>
+        <h4><SiAirbnb/></h4>
+        <h4 id='foot'>airbnb</h4>
+
+      </div>
     </div>
   );
 }
